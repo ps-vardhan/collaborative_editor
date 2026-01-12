f903baa53ec5369cf8d4c3552ac0c1fa19f8dc4f
Vardhan Reddy <peesarisaivardhan@gmail.com>
2026-01-12 17:53:27 +0530

    Code for making a default smooth Brush

---
+++ src/components/brushes/defaultBrush.js
@@ -0,0 +1,11 @@
+export const defaultBrush=(ctx,{start,end,color,size})=>{
+    ctx.strokeStyle=color;
+    ctx.lineWidth=size;
+    ctx.lineCap='round';
+    ctx.lineJoin='round';
+
+    ctx.beginPath();
+    ctx.moveTo(start.x,start.y);
+    ctx.lineTo(end.x,end.y);
+    ctx.stroke();
+};
