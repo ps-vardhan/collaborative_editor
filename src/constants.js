3a8c01979ea8115b84c0688ba199987f6c12e7ae
Vardhan Reddy <peesarisaivardhan@gmail.com>
2026-01-16 16:13:03 +0530

    Added Code_Snippets functions which produces inital hello functionality.

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,12 @@
-export const LANGUAGES_Versions = {
-    javascript:'18.15.0',
-    python:'3.10.4',
-    java:'15.0.2',
-    c:'11.2.0',
-}
+export const Language_Versions = {
+  javascript: "18.15.0",
+  python: "3.10.4",
+  java: "15.0.2",
+  c: "11.2.0",
+};
+export const Code_Snippets = {
+  javascript: `function greet(name){\n\tconsole.log("Hello,"+name+"!");\n}\n\ngreet("Guest");\n`,
+  python: `def greet(name):\n\tprint("Hello, "+name+"!")\n\ngreet("User")\n`,
+  java: `public class HelloWorld {\n\tpublic static void main(String[]args){\n\t\tSystem.out.println("Hello World");\n\t}\n}\n`,
+  c: `#include <stdio.h>\n\nint main(){\n\tprintf("Hello World");\n\treturn 0;\n}\n`,
+};
