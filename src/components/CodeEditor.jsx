7c035d3f1b89495e027d29fdfd7d9a4639024a31
Vardhan Reddy <peesarisaivardhan@gmail.com>
2026-01-20 00:14:08 +0530

    Changed position of page layout

--- src/components/CodeEditor.jsx
+++ src/components/CodeEditor.jsx
@@ -1,13 +1,19 @@
 import { Box, Button, useToast } from "@chakra-ui/react";
 import { Editor } from "@monaco-editor/react";
 import { useEffect, useRef, useState } from "react";
+import { forwardRef, useImperativeHandle } from "react";
+// import { useParams } from "react-router-dom";
+import { MonacoBinding } from "y-monaco";
+// import { WebsocketProvider } from "y-websocket";
+// import * as Y from "yjs";
 import { Code_Snippets } from "../constants";
 import LanguageSelector from "./LanguageSelector";
 import Output from "./Output";
 
-const CodeEditor = () => {
+const CodeEditor = forwardRef(({ doc, provider, language }, ref) => {
+  // const { roomId } = useParams();
   const [value, setValue] = useState("");
-  const [language, setLanguage] = useState("Select Language");
+  // const [language, setLanguage] = useState("Select Language");
   const editorRef = useRef();
 
   const [outputHeight, setOutputHeight] = useState(60);
@@ -21,11 +27,33 @@ const CodeEditor = () => {
   const onMount = (editor) => {
     editorRef.current = editor;
     editor.focus();
+
+    if (!doc || !provider) return;
+
+    // const doc = new Y.Doc();
+    // const provider = new WebsocketProvider("ws://localhost:5000", roomId, doc);
+
+    const type = doc.getText("monaco");
+
+    const binding = new MonacoBinding(
+      type,
+      editor.getModel(),
+      new Set([editor]),
+      provider.awareness,
+    );
+
+    if (language === "Select Lang") {
+      editor.setValue("// Select a Language");
+    }
   };
 
   const onSelect = (language) => {
     setLanguage(language);
     setValue(Code_Snippets[language]);
+
+    if (editorRef.current) {
+      editorRef.current.setValue(Code_Snippets[language]);
+    }
   };
 
   const startResize = (e) => {
@@ -45,6 +73,16 @@ const CodeEditor = () => {
       }
     }
   };
+
+  useEffect(() => {
+    if (editorRef.current) {
+      if (language === "Select Lang") {
+        editorRef.current.setValue("// Select a Language");
+      } else if (Code_Snippets[language]) {
+        editorRef.current.setValue(Code_Snippets[language]);
+      }
+    }
+  }, [language]);
 
   useEffect(() => {
     if (isDragging) {
@@ -79,15 +117,19 @@ const CodeEditor = () => {
       setLoading(false);
     }, 1000);
   };
+
+  useImperativeHandle(ref, () => ({
+    runCode: runCode
+  }));
+
   return (
     <Box height="100%" display="flex" flexDirection="column" overflow="hidden">
-      <Box minHeight="0" flex="1" display="flex" flexDirection="column">
+      {/* <Box minHeight="0" flex="1" display="flex" flexDirection="column">
         <Box
           display="flex"
           justifyContent="space-between"
           alignItems="center"
-          pr={4}
-        >
+          pr={4}>
           <LanguageSelector language={language} onSelect={onSelect} />
           <Button
             type="button"
@@ -96,25 +138,24 @@ const CodeEditor = () => {
             colorScheme="green"
             isLoading={loading}
             onClick={runCode}
-            mt={3}
-          >
+            mt={3}>
             Run
           </Button>
-        </Box>
+        </Box> */}
 
-        <Box flex="1" overflow="hidden">
-          <Editor
-            height="90%"
-            defaultLanguage="javascript"
-            language={language}
-            defaultValue="// Select a Language"
-            theme="vs-dark"
-            onMount={onMount}
-            value={value}
-            onChange={(val) => setValue(val)}
-          />
-        </Box>
+      <Box flex="1" overflow="hidden">
+        <Editor
+          height="100%"
+          defaultLanguage="javascript"
+          language={language}
+          defaultValue="// Select a Language"
+          theme="vs-dark"
+          onMount={onMount}
+          // value={value}
+          onChange={(val) => setValue(val)}
+        />
       </Box>
+
 
       <Box
         height="5px"
@@ -129,10 +170,10 @@ const CodeEditor = () => {
           // language={language}
           isOpen={isOutputOpen}
           logs={logs}
-          // onOpen={handleRunTrigger}
+        // onOpen={handleRunTrigger}
         />
       </Box>
-    </Box>
+    </Box >
   );
-};
+});
 export default CodeEditor;
